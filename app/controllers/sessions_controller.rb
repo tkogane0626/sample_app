6f913fb883a2bbf1aaf90363260791fec161bf2c
tkogane0626 <pad.kokin@gmail.com>
2021-06-13 18:45:13 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,23 +5,12 @@ class SessionsController < ApplicationController
   end
 
   # GET /login(.:format)
-  # def create
-  #   user = User.find_by(email: params[:session][:email].downcase)
-  #   if user&.authenticate(params[:session][:password])
-  #     log_in user
-  #     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  #     redirect_to user
-  #   else
-  #     flash.now[:danger] = 'Invalid email/password combination'
-  #     render 'new'
-  #   end
-  # end
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  # beforeフィルター
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   # GET /users/:id(.:format)
   def show
     @user = User.find(params[:id])
@@ -8,6 +13,11 @@ class UsersController < ApplicationController
   # GET /signup(.:format)
   def new
     @user = User.new
+  end
+
+  # GET /users(.:format)
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   # POST /users(.:format)
@@ -22,10 +32,51 @@ class UsersController < ApplicationController
     end
   end
 
+  # GET /users/:id(.:format)
+  def edit
+  end
+
+  # PATCH PUT /users/:id(.:format)
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  # DELETE /users/:id(.:format)
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
