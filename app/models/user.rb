fb47c6c0aedcbb09bfb10248bdbcef5a4917ae35
tkogane0626 <pad.kokin@gmail.com>
2023-04-09 23:45:48 +0900

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },
+                       allow_nil: true
 
   has_secure_password
 
@@ -30,6 +31,11 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  def session_token
+    remember_digest || remember
   end
 
   def authenticated?(remember_token)
