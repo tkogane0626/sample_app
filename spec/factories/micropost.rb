abe6b67ab4da14967057ead8f1c6b975b40d1f30
tkogane0626 <pad.kokin@gmail.com>
2023-04-28 19:26:32 +0900

    Add factory files

---
+++ spec/factories/micropost.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :micropost do
+    # Layout/EmptyLinesAroundBlockBody
+  end
+end
---
+++ spec/factories/relationship.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :relationship do
+    # Layout/EmptyLinesAroundBlockBody
+  end
+end
